0e3ed2d9905a8c1d5994af764df5305cc0d70b2e
Leticia Silva <dev.leticiasilva@gmail.com>
2024-09-30 09:22:23 +0100

    Atualizações: corrigido conflito http/https e adicionadas variáveis de ambiente

--- src/App.js
+++ src/App.js
@@ -1,12 +1,87 @@
-import { useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
 import SearchIcon from './search.svg';
 import './App.css';
 
+const apiUrl = process.env.REACT_APP_API_URL;
+const apiKey = process.env.REACT_APP_API_KEY;
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [movies, setMovies] = useState([]);
+
+  // Chama a função para buscar filmes iniciais (Harry Potter)
+  useEffect(() => {
+    searchMovies('Harry Potter');
+  }, []);
+
+  // Função de busca de filmes
+  const searchMovies = async (title) => {
+    try {
+      const response = await fetch(`${apiUrl}?apikey=${apiKey}&s=${title}`);
+      const data = await response.json();
+
+      if (data.Search) {
+        setMovies(data.Search);
+      } else {
+        setMovies([]); // Caso nenhum filme seja encontrado
+      }
+    } catch (error) {
+      console.error("Erro ao buscar filmes:", error);
+    }
+  };
+
+  return (
+    <div className="app">
+      <h1>Movies Flix</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img 
+          src={SearchIcon} 
+          alt="search" 
+          onClick={() => searchMovies(searchTerm)} 
+        />
+      </div>
+
+      {movies.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard key={movie.imdbID} movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
+
+
+/* import { useEffect, useState} from "react";
+import MovieCard from "./MovieCard";
+import SearchIcon from './search.svg';
+import './App.css'; */
+
+/* const apiUrl = process.env.REACT_APP_API_URL;
+const apiKey = process.env.REACT_APP_API_KEY;
+
+console.log(apiUrl); // http://www.omdbapi.com/
+console.log(apiKey); // key */
+
+
 
 // key: 7b41e19e
 
-const API_URL = "http://www.omdbapi.com/?apikey=7b41e19e";
+/* const API_URL = "http://www.omdbapi.com/?apikey=7b41e19e"; */
 
 
 /* const movie1 = {
@@ -14,10 +89,10 @@ const API_URL = "http://www.omdbapi.com/?apikey=7b41e19e";
     "Year" : "2012",
     "imdbID" : "tt2586634",
     "Type" : "movie",
-    "Poster" : "N/A"
+    "Poster" : "N/A"   
 } */
 
-const App= () => {
+/* const App= () => {
     const [searchTerm, setSearchTerm] = useState('');
     const [movies, setMovies] = useState([]);
 
@@ -34,11 +109,11 @@ const App= () => {
 
     setMovies(data.Search)
         
-       };
+       }; */
 
    
 
-    return (
+   /*  return (
         <div className="app">
             <h1>Movies Flix</h1>
 
@@ -54,9 +129,9 @@ const App= () => {
                 onClick={() => searchMovies(searchTerm)}           
                 />
 
-            </div>
+            </div> */
 
-            {movies?.length > 0 ? (
+    /*         {movies?.length > 0 ? (
         <div className="container">
           {movies.map((movie) => (
             <MovieCard key={movie.imdbID} movie={movie} />
@@ -70,5 +145,5 @@ const App= () => {
     </div>
   );
 };
-
-export default App;
+ */
+/* export default App; */
